e412a615707cbd87fa4f4a9d0846254da2fe3c01
Rarivoson ANDRIANAMBININTSOA <nambinina1@corp.telma.mg>
2021-06-23 19:30:48 +0300

    v0.0.6 - Fix API endpoint for authentication

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
+import apiConfig from "../config/api.config"
 import {
   GET_ERRORS,
   SET_CURRENT_USER,
   USER_LOADING
 } from "./types";
 
+const BASE_URL = apiConfig.BASE_URL;
+
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("/api/users/register", userData)
+    .post(BASE_URL + "/users/register", userData)
     .then(res => history.push("/login")) // re-direct to login on successful register
     .catch(err =>
       dispatch({
@@ -23,7 +26,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("/api/users/login", userData)
+    .post(BASE_URL + "/users/login", userData)
     .then(res => {
       // Save to localStorage// Set token to localStorage
       const { token } = res.data;
